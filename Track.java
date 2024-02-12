dd2dc79c4564d294a2d7dd9acd7ceb0d30777b61
Yarden Eshel <yardenyarden5@gmail.com>
2024-02-12 19:45:19 +0200

    finisihed

--- Track.java
+++ Track.java
@@ -42,7 +42,8 @@ class Track {
     // Returns a string that represents the totalSeconds as "minutes:seconds",
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
-        //// replace the following statement with your code
-        return "";
+        int minutes = totalSeconds/60;
+        int seconds = totalSeconds - minutes*60;
+        return String.format("%s:%02s", minutes,seconds);
     }
 }
